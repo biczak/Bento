bc428b514b9bce68e911be5464e7ae755f124a5d
Alex Biczak <25611530+biczak@users.noreply.github.com>
2021-11-18 07:42:25 -0500

    styles2

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -5,19 +5,20 @@
 // Print cards
 const printCards = () => {
   for (const card of CONFIG.cards) {
-    
     // Card Item
     let item = `
         <a
           href="${card.link}"
-          target="${CONFIG.openInNewTab ? '_blank' : ''}"
+          target="${CONFIG.openInNewTab ? "_blank" : ""}"
           class="buttonLink__link card buttonLink__link-${card.id}"
         >
-          <i class="buttonLink__icon" data-feather="${card.icon}"></i>
+          <i class="buttonLink__icon ${card.icon}" data-feather="${
+      card.icon
+    }"></i>
         </a>
     `;
 
-    const position = 'beforeend';
+    const position = "beforeend";
 
     buttonsContainer.insertAdjacentHTML(position, item);
   }
